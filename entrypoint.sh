6cf74e14de6d7e8fe2214333d57b871228425427
haya14busa <haya14busa@gmail.com>
2020-12-30 16:03:20 +0000

    Redirect markdownlint stderr to stdout

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ cd "${GITHUB_WORKSPACE}" || exit
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-markdownlint "${INPUT_MARKDOWNLINT_FLAGS:-.}" |
+markdownlint "${INPUT_MARKDOWNLINT_FLAGS:-.}" 2>&1 |
   reviewdog \
       -efm="%f:%l:%c %m" \
       -efm="%f:%l %m" \
