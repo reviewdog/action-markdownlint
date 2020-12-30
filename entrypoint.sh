59c31dd69c3385d3dacce6b774ae5dc8a9a4971d
haya14busa <haya14busa@gmail.com>
2020-12-30 15:29:19 +0000

    Fix markdownlint
    - fix the errorformat
    - fix the reporter condition
    - remove needless -diff flag
    - remove needless -tee flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,5 @@ cd "${GITHUB_WORKSPACE}" || exit
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-if [ "${INPUT_REPORTER}" = x"github-pr-review" ]; then
-  markdownlint "${INPUT_MARKDOWNLINT_FLAGS:-.}" |
-    reviewdog -efm="%s:%l:%c %m" -name="markdownlint" -diff="git diff HEAD^" -reporter=github-pr-review -level="${INPUT_LEVEL}" -tee
-else
-  markdownlint "${INPUT_MARKDOWNLINT_FLAGS:-.}" |
-    reviewdog -efm="%f:%l:%c %m" -name="markdownlint" -diff="git diff HEAD^" -reporter=github-pr-check -level="${INPUT_LEVEL}" -tee
-fi
+markdownlint "${INPUT_MARKDOWNLINT_FLAGS:-.}" |
+  reviewdog -efm="%f:%l:%c %m" -efm="%f:%l %m" -name="markdownlint" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
